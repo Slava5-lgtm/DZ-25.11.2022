883e892cd2797a251ffee581773d7243570f433d
Vaceslav <vaceslavsergunin@gmail.com>
2022-11-24 21:26:06 +0600

    добавил програму

---
+++ Task5.py
@@ -0,0 +1,12 @@
+# - A (3,6); B (2,1) -> 5,09
+# - A (7,-5); B (1,-1) -> 7,21
+
+# print(type(a)) # вывести на экран и показать кокой будет тип даных
+# тип данных и переменная
+# int, float, boolean, str, list, None
+
+
+x_coordinate_A = float(input('Введите координату точки А по оси Х: '))
+y_coordinate_A = float(input('Введите координату точки А по оси Y: '))
+x_coordinate_B = float(input('Введите координату точки B по оси Х: '))
+y_coordinate_B = float(input('Введите координату точки B по оси Y: '))
