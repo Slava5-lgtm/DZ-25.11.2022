d36d0da6af5a81b9aa27a4c0a9c7bc135a3e52b5
Vaceslav <vaceslavsergunin@gmail.com>
2022-11-25 00:26:28 +0600

    добавил задачу

--- Task5.py
+++ Task5.py
@@ -6,7 +6,25 @@
 # int, float, boolean, str, list, None
 
 
-x_coordinate_A = float(input('Введите координату точки А по оси Х: '))
-y_coordinate_A = float(input('Введите координату точки А по оси Y: '))
-x_coordinate_B = float(input('Введите координату точки B по оси Х: '))
-y_coordinate_B = float(input('Введите координату точки B по оси Y: '))
+# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+# ними в 2D пространстве.
+
+# Пример:
+
+# - A (3,6); B (2,1) -> 5,09
+# - A (7,-5); B (1,-1) -> 7,21
+
+
+import math
+print('Введите координату точки A')
+numberAX = float(input('X: '))
+numberAY = float(input('Y: '))
+
+print('Введите координату точки')
+numberBX = float(input('X: '))
+numberBY = float(input('Y: '))
+a = numberAX - numberBX
+b = numberAY - numberBY
+c = a * a + b * b
+sqrt = round(math.sqrt(c), 3)
+print(sqrt)
